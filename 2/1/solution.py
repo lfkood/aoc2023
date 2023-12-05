4a902e221e70b10bf3e8eaf884d679f394be005b
lfkood <laidofabian@gmail.com>
2023-12-05 22:05:13 +0200

    2nd day part 1

--- 2/1/solution.py
+++ 2/1/solution.py
@@ -1,6 +1,6 @@
 import re
 
-with open("input.txt", "r") as file:
+with open("2.txt", "r") as file:
     input_string = file.read()
 
 red_max = 12
@@ -8,17 +8,29 @@ green_max = 13
 blue_max = 14
 sum = 0
 
+def cast_to_int_and_sort(list):
+    new_list = []
+    for x in list:
+        new_list.append(int(x))
+        new_list.sort(reverse=True)
+    return new_list
 for line in input_string.splitlines():
-    a = re.search("(\d+):", line)
-    reds = re.findall("(/d+) red", line).sort(True)
-    greens = re.findall("(/d+) green", line).sort(True)
-    blues = re.findall("(/d+) blue", line).sort(True)
+    a = re.findall("(\d+):", line)
+    
+    reds = cast_to_int_and_sort(re.findall("(\d+) red", line))
+    greens = cast_to_int_and_sort(re.findall("(\d+) green", line))
+    blues = cast_to_int_and_sort(re.findall("(\d+) blue", line))
 
-    if reds[0] > red_max:
-        continue
-    elif greens[0] > green_max:
-        continue
-    elif blues [0] > blue_max:
-        continue
+    if reds != None:
+        if int(reds[0]) > red_max:
+            continue
+    if greens != None:
+        if int(greens[0]) > green_max:
+            continue
+    if blues != None:
+        if int(blues[0]) > blue_max:
+            continue
 
-    sum += int(a)
+    sum += int(a[0])
+
+print(sum)
